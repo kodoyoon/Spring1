088b9f4a4ebce55c326b2498450b467cd2fe6c24
doyoon <doyoun0527@naver.com>
2024-04-13 19:17:04 +0900

    회원 서비스 개발

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -4,22 +4,32 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
+
 
 public class MemberService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
-    //  회원가입
+    /**
+     * 회원가입 */
     public Long join(Member member) {
-        //같은 이름이 있는 중복회원은 x
-
-        Optional<Member> result = memberRepository.findByName(member.getName());
-        result.ifPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다. ");
-        });
-
-
-        memberRepository.save(member);
+        validateDuplicateMember(member); //중복 회원 검증 memberRepository.save(member);
         return member.getId();
     }
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+
+    //전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
 }
+
